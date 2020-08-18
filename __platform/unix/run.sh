ec47324dd22cbdb400735fad27c45bafd06e99df
zsh2401 <zsh2401@163.com>
2020-08-18 08:14:56 +0800

    staable

--- __platform/unix/run.sh
+++ __platform/unix/run.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/bash
-echo "==============================="
-echo "0. This script can only use on Linux which installed nvidia official driver and optirun command."
-echo "1. To fix input method,read this:https://www.mcbbs.net/thread-728252-1-4.html"
-echo "==============================="
-
-# If you have installed nvidia driver and optirun.
-optirun java -jar HMCL.jar
-# ELSE
-#java -jar HMCL.jar
-
-# AMD GPU should also have some way to run minecraft on gpu.
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb1bdef5266308d6f25030ac8ec5f0507c2dee243705bfc32be400c452a4ee77
+size 463
